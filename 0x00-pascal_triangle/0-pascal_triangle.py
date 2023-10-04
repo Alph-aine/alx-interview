adc898dd5df0a65d355ba88cbd26230a1ee0f8b9
Abdulbasit <aabdulrafiu721@gmail.com>
2023-10-04 02:14:50 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+A function that implements pascal triangle in python,
+'''
+
+
+def pascal_triangle(n):
+    '''pascal triangle algorithm'''
+    if n <= 0:
+        return []
+    triangle = []
+    for row_number in range(n):
+        current_row = [1]
+        if triangle:
+            prev_row = triangle[-1]
+            for i in range(1, len(prev_row)):
+                new_value = prev_row[i] + prev_row[i - 1]
+                current_row.append(new_value)
+            current_row.append(1)
+        triangle.append(current_row)
+    return triangle
+
+
+# Example usage:
+result = pascal_triangle(3)
+for row in result:
+    print(row)
